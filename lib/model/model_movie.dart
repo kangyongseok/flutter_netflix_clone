5c6a6ce380760ace6a36f3821400a50c225d0468
강용석 <kangyongsuek@gmail.com>
2020-04-11 23:24:02 +0900

    modify: 실습예제따라 ui 코드작성중

--- lib/model/model_movie.dart
+++ lib/model/model_movie.dart
@@ -1,3 +1,5 @@
+// 영화 모델 설정
+// 영화 데이터 관리를 쉽게 하기 위함
 class Movie {
   final String title;
   final String keyword;
@@ -5,10 +7,10 @@ class Movie {
   final bool like;
 
   Movie.fromMap(Map<String, dynamic> map)
-  :title = map['title'],
-  keyword = map['keyword'],
-  poster = map['poster'],
-  like = map['like']
+    :title = map['title'],
+      keyword = map['keyword'],
+      poster = map['poster'],
+      like = map['like'];
 
   @override
   String toString() => "Movie<$title:$keyword>";
